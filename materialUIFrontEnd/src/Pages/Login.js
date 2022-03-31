4d40203c4342ddda7bd369c5eecab6c0040387bd
Hector <hector.gonzalez@utexas.edu>
2022-03-30 21:43:10 -0500

    Added home page

--- materialUIFrontEnd/src/Pages/Login.js
+++ materialUIFrontEnd/src/Pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TextField, Button, InputAdornment, Grid, Typography, makeStyles } from "@material-ui/core";
+import {TextField, Button, InputAdornment, Grid, Typography, makeStyles, Box } from "@material-ui/core";
 import "./Profile.css";
 import axios from "axios"
 import AccountBoxRoundedIcon from '@material-ui/icons/AccountBoxRounded'
@@ -9,23 +9,27 @@ import ArrowRightRoundedIcon from '@material-ui/icons/ArrowRightRounded';
 
 const useStyles = makeStyles({
     text: {
-        marginTop: 20,
-        marginBottom: 130,
-        display: 'block',
+      marginTop: 20, 
+      marginBottom: 130,
+      display: 'block',
     },
 
     userField: {
         marginBottom: 10,
-        display: 'block'
+        display: 'block',
     },
 
     passwordField: {
         marginBottom: 45,
-        display: 'block'
-    }
-})
+        display: 'block',
+    },
 
-export default function Login() {
+    button: {
+        borderRadius: 10,
+      }, 
+  })
+
+export default function Login(){
 
     const classes = useStyles()
     const [username, setUser] = useState('')
@@ -39,96 +43,120 @@ export default function Login() {
         setUserError(false)
         setPassError(false)
 
-        if (username === '') {
+        if (username == ''){
             setUserError(true)
         }
 
-        if (password === '') {
+        if (password == ''){
             setPassError(true)
         }
 
-        if (username && password) {
+        if (username && password){
             // do something with form values, and then
             axios.post("https://www.pythonurl.herokuapp.com", {
                 user: username,
                 pass: password
             })
-                .then((response) => {
-                    console.log(response);
-                }, (error) => {
-                    console.log(error);
-                });
+            .then((response) => {
+                console.log(response);
+            }, (error) => {
+                console.log(error);
+            });
             //we have to then return the profile depending on the log in information here.
         }
     }
 
-    return (
-        <Grid
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-        >
+        return (
+            <Box id="container">
+                <Box id="welcome-text" textAlign="center">
+                    <Typography 
+                        variant='h1'
+                        className={classes.text}
+                    >
+                        Welcome Back!
+                    </Typography>
+                </Box>
 
-            <Typography
-                variant='h1'
-                className={classes.text}
-            >
-                Welcome
-            </Typography>
+                <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+                    <Box id="user-input" alignItems="center" px={55}>
+                        <TextField 
+                            className={classes.userField} 
+                            label="Username"
+                            color="secondary"
+                            onChange={
+                                (e) => setUser(e.target.value)
+                            }
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <AccountBoxRoundedIcon />
+                                    </InputAdornment>
+                                ),
+                                style: {
+                                    fontSize: 24,
+                                },
+                            }} 
+                            InputLabelProps={{ 
+                                style: { fontSize: 22 } 
+                            }}
+                            required
+                            fullWidth
+                            error={userError}
+                        />
+                    </Box>
 
+                    <Box id="pass-input" alignItems="center" px={55}>
+                        <TextField
+                            className={classes.passwordField}
+                            type="password"
+                            label="Password"
+                            color="secondary"
+                            onChange={
+                                (e) => setPass(e.target.value)
+                            }
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <VpnKeyRounded />
+                                    </InputAdornment>
+                                ),
+                                style: {
+                                    fontSize: 24,
+                                },
+                            }} 
+                            InputLabelProps={{ 
+                                style: { fontSize: 22 } 
+                            }}
+                            autoComplete="current-password"
+                            required
+                            fullWidth
+                            error={passError}
+                        />
+                    </Box>
 
-            <form noValidate autoComplete='off' onSubmit={handleSubmit}>
-                <TextField
-                    id="user-input"
-                    className={classes.userField}
-                    label="Username"
-                    color="secondary"
-                    onChange={
-                        (e) => setUser(e.target.value)
-                    }
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <AccountBoxRoundedIcon />
-                            </InputAdornment>
-                        )
-                    }}
-                    required
-                    //fullWidth
-                    style={{ width: '200%' }}
-                    error={userError}
-                />
-                <TextField
-                    id="password-input"
-                    className={classes.passwordField}
-                    type="password"
-                    label="Password"
-                    color="secondary"
-                    onChange={
-                        (e) => setPass(e.target.value)
-                    }
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <VpnKeyRounded />
-                            </InputAdornment>
-                        )
-                    }}
-                    autoComplete="current-password"
-                    required
-                    fullWidth
-                    error={passError}
-                />
-                <Button
-                    variant="contained"
-                    type="submit"
-                    endIcon={<ArrowRightRoundedIcon fontSize='large' />}
-                >
-                    Login
-                </Button>
-            </form>
-        </Grid>
-    );
+                    <Box id="login-button/signup" alignItems="center" pl={65}>
+                        <Button
+                            id="login"
+                            className={classes.button} 
+                            variant="contained"
+                            type="submit"
+                            endIcon={<ArrowRightRoundedIcon fontSize='large'/>}
+                            style={{ fontSize: 18, maxHeight: 30 }}
+                        >
+                            Login
+                        </Button>
+
+                        <Button
+                            id="signup"
+                            className={classes.button}
+                            variant="contained"
+                            endIcon={<ArrowRightRoundedIcon fontSize='large'/>}
+                            style={{ fontSize: 18, maxHeight: 30 }}
+                        >
+                            New User?
+                        </Button>
+                    </Box>
+                </form>
+            </Box>
+        );
 }
